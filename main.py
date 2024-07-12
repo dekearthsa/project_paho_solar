9870cf0bb41b0cbeb01bfa93258043e6b34572ff
pcsishun <pcsishun@gmail.com>
2024-07-12 11:03:26 +0700

    update

--- main.py
+++ main.py
@@ -2,22 +2,24 @@ import paho.mqtt.client as mqtt
 import json
 ## IPv4 192.168.1.107
 
+HOST_MQTT = "localhost"
+PORT_MQTT = 1883
+PUB_MQTT = "test/pub/topic"
+SUB_MQTT = "test/topic"
+
 def on_connect(client, userdata, flags, rc):
     print(f"Connected with result code {rc}")
-    client.subscribe("test/topic")
+    client.subscribe(SUB_MQTT)
 
 def on_message(client, userdata, msg):
-    # print(f"{msg.topic} {msg.payload}")
     load_json_data = json.loads(msg.payload)
-    # print("load_json_data => ", load_json_data)
     print(load_json_data['deviceID'])
     if load_json_data['deviceID'] == "test":
-        client.publish("test/pub/topic", "Hello MQTT")
+        client.publish(PUB_MQTT, "Hello MQTT")
 
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
-# client.on_publish = on_publish
 client.connect("localhost", 1883, 60)
 client.loop_forever()
 
